5f5fb98a7e84624d594724e543c58a40b024d229
MilkTablee <119823959+MilkTablee@users.noreply.github.com>
2024-07-17 23:36:55 +0100

    Crafted an interface for User account registration and login. Currently implementing general response.

---
+++ ServerLibrary/Repositories/Contracts/IUserAccount.cs
@@ -0,0 +1,10 @@
+﻿using BaseLibrary.DTOs;
+
+namespace ServerLibrary.Repositories.Contracts
+{
+    public interface IUserAccount
+    {
+        Task<GeneralResponse> CreateAsync(Register user);
+        Task<LoginResponse> SignInAsync(Login user);
+    }
+}
